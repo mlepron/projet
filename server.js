7a0b84efd816ed9c0c808438fbc571e345d6a3e4
LEPRON Mathieu <lepron@canette.iutrs.unistra.fr>
2015-09-16 16:12:14 +0200

    Ajout de l'utilisation de HTTPS au serveur

---
+++ server.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var https = require('https');
+var express = require('express');
+var app = express();
+
+var key = fs.readFileSync('./key.pem', 'utf8');
+var cert = fs.readFileSync('./server.crt', 'utf8');
+
+var https_options = {
+	key: key,
+	cert: cert
+};
+
+
+app.get('/', function (req, res) {
+  res.sendfile('public/index.html');
+});
+
+var server = https.createServer(https_options, app).listen(5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
